e69e75d07bd3415c5710de8634a5964d1020a185
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-10-02 21:07:40 -0300

    Add Swagger UI

---
+++ src/main/java/me/dio/cart/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package me.dio.cart.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket getBean() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .paths(PathSelectors.regex("/ifood-dev-week/.*"))
+                .build()
+                .apiInfo(getInfo());
+    }
+    private ApiInfo getInfo() {
+        return new ApiInfoBuilder()
+                .title("Cart API")
+                .description("Cart API to serve a delivery application")
+                .build();
+    }
+}
